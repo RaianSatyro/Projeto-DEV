3de1307950000932a3b756be018f0ae9c2bd22b9
Francisco <four4kcod@gmail.com>
2024-03-26 20:51:06 -0300

    tirei coisa inuteis

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,6 @@ document.getElementById("registrationForm").addEventListener("submit", async fun
     const email = document.getElementById("form-email").value.trim();
     const senha = document.getElementById("form-senha").value.trim();
 
-    if (!nome || !sobrenome || !email || !senha) {
-        alert('Por favor, preencha todos os campos.');
-        return;
-    }
-
     try {
         await subfofoq({ nome, sobrenome, email, password: senha });
         console.log('Inscrição enviada com sucesso.');
