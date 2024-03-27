f7f40184d7db8736a787485e053bff78532aa413
RaianSatyro <raiansatyro45@gmail.com>
2024-03-27 17:20:03 -0300

    aplicando o firebase auth no codigo

---
+++ src/js/firebase/firebase-auth.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4s1zx9j47X3nT9njP7jS7FNbImdnk_a4",
+    authDomain: "login-user-6e7fa.firebaseapp.com",
+    projectId: "login-user-6e7fa",
+    storageBucket: "login-user-6e7fa.appspot.com",
+    messagingSenderId: "388334286783",
+    appId: "1:388334286783:web:4ca7f6f12a03e757a1130a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export function login() {
+    signInWithEmailAndPassword(auth, 'satyro45@gmail.com', '1234567')
+        .then((userCredential) => {
+            // Usuário autenticado com sucesso
+            const user = userCredential.user;
+            console.log('Login realizado com sucesso', user);
+        })
+        .catch((error) => {
+            // Ocorreu um erro durante a autenticação
+            console.error('Erro ao fazer login:', error.message);
+        });
+}
+
+// Chama a função login quando a página é carregada
+
--- src/js/validations.js
+++ src/js/validations.js
@@ -1,20 +1,35 @@
-
-function validatefield(){
-    const emailValid = isEmailValid();
-    document.querySelector('#recupera-senha').disablad = !emailValid;
-}
-
-
-function isEmailValid(){
-    const email = document.querySelector('#form-email').value;
-    if(!email){
-        console.log('falso');
-        return false;
+function validarCampos() {
+    const emailValido = verificarEmail();
+    const botaoRecuperaSenha = document.querySelector('#recupera-senha');
+    botaoRecuperaSenha.disabled = !emailValido;
+    if (!emailValido) {
+        botaoRecuperaSenha.classList.add('botao-invalido');
+    } else {
+        botaoRecuperaSenha.classList.remove('botao-invalido');
     }
-    console.log('falso');
-    return validateEmail(email);
 }
 
-function validateEmail(email){
+function verificarEmail() {
+    const email = document.querySelector('#form-email').value;
+    if (!email) {
+        console.log('E-mail vazio');
+        return false; // Retorna falso se o campo estiver vazio
+    }
+
+    const formatoValido = validarFormatoEmail(email);
+    if (!formatoValido) {
+        console.log('Formato de e-mail inválido');
+        return false; // Retorna falso se o formato do e-mail for inválido
+    }
+
+    console.log('E-mail válido');
+    return true; // Retorna verdadeiro se o e-mail for válido
+}
+
+function validarFormatoEmail(email) {
     return /\S+@\S+/.test(email);
 }
+
+
+// Chama a função validarCampos quando o valor do campo de e-mail muda
+document.querySelector('#form-email').addEventListener('input', validarCampos);
