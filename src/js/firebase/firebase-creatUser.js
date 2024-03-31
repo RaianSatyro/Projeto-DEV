ce01fc823021dd40568227461be3f4c96db7a69e
Francisco <four4kcod@gmail.com>
2024-03-31 18:06:14 -0300

    Feedback na hr de aperta o botao de fazer a conta feito

--- src/js/firebase/firebase-creatUser.js
+++ src/js/firebase/firebase-creatUser.js
@@ -1,23 +1,30 @@
-import appAuth from './fireBase-InitAuth.js'
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
-
+import appAuth from './fireBase-InitAuth.js';
 
 const auth = getAuth(appAuth);
 
 function createUser(userEmail, userPass) {
     createUserWithEmailAndPassword(auth, userEmail, userPass)
-      .then((userCredential) => {
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Usuário cadastrado com sucesso:", user.uid);
+            mostrarRegistroSucessoDialog();
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`Erro ao cadastrar usuário: ${errorCode} ${errorMessage}`);
+        });
+}
 
-        const user = userCredential.user;
-        console.log("Usuário cadastrado com sucesso:", user.uid);
-        window.location.href = "/src/pages/cadastro-sucess.html";
-      })
-      .catch((error) => {
-        // Erro ao criar o usuário
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(`Erro ao cadastrar usuário: ${errorCode} ${errorMessage}`);
-      });
-  }
-  
-  export { createUser };
+function mostrarRegistroSucessoDialog() {
+  const dialog = document.getElementById("registroSucessoDialog");
+  dialog.style.display = "block";
+  setTimeout(() => {
+      dialog.style.opacity = "1"; // Altera a opacidade para 1
+      dialog.style.transform = "translateY(0)"; // Remove a transformação translateY
+  }, 50); // Adiciona um pequeno atraso para garantir que a transição seja aplicada corretamente
+}
+
+
+export { createUser, mostrarRegistroSucessoDialog };
